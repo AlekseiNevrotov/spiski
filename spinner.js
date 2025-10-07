9c4915010ec33e9b054395c71920ce4416e1948b
Алексей Невротов <elazarsound@gmail.com>
2025-10-07 17:42:10 +0300

    spiski

---
+++ spinner.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('myForm');
+        const spinner = document.getElementById('spinner');
+        form.addEventListener('submit', function(event) {
+            spinner.style.display = 'block';
+        });
