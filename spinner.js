8c7caf872bc07e74e856197fd2e80523c4fba94d
Алексей Невротов <elazarsound@gmail.com>
2025-10-07 18:03:35 +0300

    script

--- spinner.js
+++
@@ -1,5 +0,0 @@
-const form = document.getElementById('myForm');
-        const spinner = document.getElementById('spinner');
-        form.addEventListener('submit', function(event) {
-            spinner.style.display = 'block';
-        });
